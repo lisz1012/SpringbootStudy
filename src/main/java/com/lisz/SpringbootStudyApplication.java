5f9b45f6f6c3208e6d964d9d0196be2f3ac99e02
lisz1012 <lisz1012@163.com>
2020-06-10 23:39:42 -0700

    Add swagger dependency and test

--- src/main/java/com/lisz/SpringbootStudyApplication.java
+++ src/main/java/com/lisz/SpringbootStudyApplication.java
@@ -1,10 +1,12 @@
 package com.lisz;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableSwagger2Doc
 public class SpringbootStudyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lisz/controller/HelloController.java
+++ src/main/java/com/lisz/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.lisz.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello xiaolu!";
+    }
+
+    @PostMapping("/hello-world")
+    public String hellowWorld() {
+        return "hellow world!";
+    }
+
+    @PutMapping("/put")
+    public String put (){
+        return "put";
     }
 }
