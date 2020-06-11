1e30d2d729fe8b354b676bd4f4009f0b753206ae
lisz1012 <lisz1012@163.com>
2020-06-10 18:02:26 -0700

    Starter

--- Readme.md
+++ Readme.md
@@ -2,4 +2,7 @@
 
 寻找各种Spring starter相关的依赖：  
 https://docs.spring.io/spring-boot/docs/2.3.0.RELEASE/reference/html/using-spring-boot.html#using-boot-starter
-直接把opm中的dependency粘过来，到我们的pom文件中就可以了
+直接把opm中的dependency粘过来，到我们的pom文件中就可以了  
+
+启动类SpringbootStudyApplication的位置是不能随便放置的，改变路径的时候要加一个配置项, 最好不要动它  
+可以加上@ComponentScan("com.lisz")让Springboot扫描到，但是一般不去动启动类的存放位置
--- src/main/java/com/lisz/controller/HelloController.java
+++ src/main/java/com/lisz/controller/HelloController.java
@@ -1,13 +1,12 @@
 package com.lisz.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "hello";
     }
