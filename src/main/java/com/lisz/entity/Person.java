c8efaa91d51233dc08ce797e13fcc2f1bf5dead3
lisz1012 <lisz1012@163.com>
2021-01-10 21:19:04 -0800

    添加实体类、配置多环境

--- src/main/java/com/lisz/entity/Person.java
+++ src/main/java/com/lisz/entity/Person.java
@@ -3,20 +3,40 @@ package com.lisz.entity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Email;
 import java.util.List;
 
 @Component
 @ConfigurationProperties(prefix = "person")
+@Validated
 public class Person {
-	//@Value("person.name")
+	//@Value("${person.name}")
 	private String name;
 	//@Value("${person.age}")
 	private Integer age;
-	//@Value("person.gender")
+	//@Value("${person.gender}")
 	private String gender;
-	//@Value("${person.likes}")
+	//@Value("#{'${person.likes}'.split(',')}")
 	private List<String> likes;
+	// @ConfigurationProperties 支持下划线到驼峰标识的转换
+	private String lastName;
+	// @Email的同时，在类上面还要加@Validate
+	@Email
+	private String email;
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				", gender='" + gender + '\'' +
+				", likes=" + likes +
+				", lastName='" + lastName + '\'' +
+				", email='" + email + '\'' +
+				'}';
+	}
 
 	public String getName() {
 		return name;
@@ -50,13 +70,19 @@ public class Person {
 		this.likes = likes;
 	}
 
-	@Override
-	public String toString() {
-		return "Person{" +
-				"name='" + name + '\'' +
-				", age=" + age +
-				", gender='" + gender + '\'' +
-				", likes=" + likes +
-				'}';
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
